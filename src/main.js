c7d9c06940c74fb922593f3d929f6caf7eb9d107
shihaidong <17335802344@163.com>
2021-08-26 23:44:51 +0800

    login

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './permission'
+import ElementUI from 'element-ui'
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+import 'element-ui/lib/theme-chalk/index.css';
+import './styles/element-variables.scss'
+import '@/styles/index.scss' // global css
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
